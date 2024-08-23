ea75abecfed064731854d5198eabd0d1394bcf35
chacronemed <entchacrone@gmail.com>
2024-08-23 21:57:35 +0100

    resolving the serachParam problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      };
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-    
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+    missingSuspenseWithCSRBailout: false,  // Add this line
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],  // Example domain
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+}
+
+module.exports = nextConfig;
